c5e786090edd2e86f610f9e8330059c93e7e8770
Ivan <ivndev0@gmail.com>
2022-07-05 22:36:58 -0300

    Criação do get e postOne by ID

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -4,7 +4,7 @@ const Task = require ('../models/Task') // quando importamos a tarefa aqui, nós
 const getAllTasks = async (req, res) => {
   try { 
     const tasksList = await Task.find() // Pq do [await]? Mais uma vez a async function precisa de aguardar a função realizar alguma tarefa e por fim retornar.
-    return res.render("index", {tasksList})
+    return res.render("index", {tasksList, task: null})
   }
   catch {
     res.status(500).send({error: err.message})
@@ -12,21 +12,44 @@ const getAllTasks = async (req, res) => {
     return res.render("index");
 };
 
-const createTask = async (req, res) => { 
+const createTask = async (req, res) => 
+{ 
     const task = req.body;
 
     if(!task.task){ // Se não tiver nada na task, o que vai acontecer? 
         return res.redirect("/") // Respondendo o comnentário acima, ele irá recarregar a página. 
     }
-    try {
+    try{
         await Task.create(task)
         return res.redirect("/")
-    } catch (err){ //sem o catch, o try não roda
+    } 
+    catch (err){ //sem o catch, o try não roda
         res.status(500).send({
             error: err.message
         })
     }
 }
+const getById =  async (req, res) => 
+{
+    try {
+        const task = await Task.findOne({_id:req.params.id}); //Filho, espera a TASK, e dai eu farei um findOne. Lá na lista, faz um findOne num ID que eu to passando. 
+        const tasksList = await Task.find();
+        res.render("index", { task, tasksList });
+    } catch (err) {
+    res.status(500).send({ error:err.message })    
+    }
+};
+const updateOneTask = async (req, res) =>
+{
+    try {
+        const task = req.body
+        await Task.updateOne({ _id: req.params.id }, task) // Ele procura um ID que vem do parametro. Achou? Dai ele altera o objeto!
+        res.redirect("/")
+    } catch (err) {
+        res.status(500).send({error:err.message})
+    }
+}
+
 /* Pq estou utilizando uma FUNÇÃO ASSíNCRONA? 
    Bom, se não utilizar este recurso o código procedural irá executar tudo pela frente, 
    sem esperar outros trechos/linhas de código, e neste caso iremos aguardar */
@@ -34,5 +57,7 @@ const createTask = async (req, res) => {
 module.exports = {
     getAllTasks,
     createTask,
+    getById,
+    updateOneTask,
 };
 
--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const TaskController = require("../controller/TaskController")
 
 routes.get("/", TaskController.getAllTasks) // rota para nos retornar [get] todas as tasks
 routes.post("/create", TaskController.createTask) //rota para criar [post] as tasks
-
+routes.get("/getById/:id", TaskController.getById) //rota para nos retornar com [get] através do ID 
+routes.post("/updateOne/:id", TaskController.updateOneTask) //rota para atualizar uma tarefa. Usamos apenas get e post pois nesta estrutura MVC, utilizando ejs, talvez estejamos limitados
 
 module.exports = routes
